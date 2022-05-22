9d91beadc22fee606a47a343bca42cc26528644d
SGNAYAK97 <sgnayak136@gmail.com>
2022-05-22 20:56:48 +0530

    smoke changes by Commando

--- Application.java
+++ Application.java
@@ -7,3 +7,23 @@
       </script>      
    </body>
 </html>
+smoke changes made by SG for new operation 
+<html>
+   <head>     
+      <script type = "text/javascript">
+         <!--
+            function getValue() {
+               var retVal = prompt("Enter your name : ", "your name here");
+               document.write("You have entered : " + retVal);
+            }
+         //-->
+      </script>      
+   </head>
+   
+   <body>
+      <p>Click the following button to see the result: </p>      
+      <form>
+         <input type = "button" value = "Click Me" onclick = "getValue();" />
+      </form>      
+   </body>
+</html>
